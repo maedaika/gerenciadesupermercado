2de386187161511330b7c9eed6b16a82e5ee8d1c
Maeda <allesjp@gmail.com>
2020-12-18 00:00:48 -0300

    subindo projeto para git

---
+++ caixa/src/main/java/br/com/caixa/App.java
@@ -0,0 +1,13 @@
+package br.com.caixa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/br/com/gerenciadesupermercado/App.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadesupermercado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
